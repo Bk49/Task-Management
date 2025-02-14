49902d184ea4db755322cebc097ed24a6c94f52d
Eric Ng <eric.ng.2022@smu.edu.sg>
2025-02-14 13:05:42 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const App = () => {
+    return (
+        <Typography variant="h1" sx={{ fontSize: "2rem", fontWeight:"bold" }}>
+            Task Management App
+        </Typography>
+    );
+};
+
+export default App;
