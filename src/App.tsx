ac0579abe69dff3961c07cd21600d463cebb4ebc
Eric Ng <eric.ng.2022@smu.edu.sg>
2025-02-14 13:36:29 +0800

    Milestone 1 basic completion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,62 @@
-import { Typography } from "@mui/material";
+import {
+    Button,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+    Typography,
+} from "@mui/material";
+import tasks from "./data/task.json";
+
+type TaskItem = {
+    id: number;
+    title: string;
+    status: "in_progress" | "not_started" | "completed";
+    priority: "high" | "none" | "medium" | "low" | "urgent";
+};
 
 const App = () => {
     return (
-        <Typography variant="h1" sx={{ fontSize: "2rem", fontWeight:"bold" }}>
-            Task Management App
-        </Typography>
+        <>
+            <header>
+                <Typography
+                    variant="h1"
+                    sx={{ fontSize: "2rem", fontWeight: "bold" }}
+                >
+                    Task Management App
+                </Typography>
+            </header>
+            <Button variant="contained">Create Task</Button>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        {["ID", "Title", "Status", "Priority"].map((label) => (
+                            <TableCell key={label} aria-label={`${label}-h`}>
+                                <Typography
+                                    variant="subtitle1"
+                                    fontWeight={800}
+                                >
+                                    {label}
+                                </Typography>
+                            </TableCell>
+                        ))}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {(tasks as TaskItem[]).map(
+                        ({ id, title, status, priority }) => (
+                            <TableRow key={id}>
+                                <TableCell>{id}</TableCell>
+                                <TableCell>{title}</TableCell>
+                                <TableCell>{status}</TableCell>
+                                <TableCell>{priority}</TableCell>
+                            </TableRow>
+                        )
+                    )}
+                </TableBody>
+            </Table>
+        </>
     );
 };
 
